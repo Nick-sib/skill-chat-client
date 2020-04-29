521c498b035da8d3069bcf4690c33f696d0c2d55
Nick-sib <58276863+Nick-sib@users.noreply.github.com>
2020-04-29 15:22:18 +0700

    Update UserEvents.java

--- src/main/java/com/example/cryptochat/UserEvents.java
+++ src/main/java/com/example/cryptochat/UserEvents.java
@@ -1,6 +1,6 @@
 package com.example.cryptochat;
 
 public interface UserEvents {
-    void UserCONNECED(long userID, String userName, int totalCount);
-    void UserDISCONNECED(int totalCount);
+    void UserCONNECED(final String userName, final int totalCount);
+    void UserDISCONNECED(final int totalCount);
 }
